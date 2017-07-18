28af12de8a9b27be40682079581fae1412d1fa37
hissa <hissamssa7@gmail.com>
2017-07-19 04:36:24 +0900

    いい感じ

--- App.js
+++ App.js
@@ -1,7 +1,353 @@
 class App{
     static main(){
-        console.log("hello, world!");
+        App.table = new FieldTable(10, 20);
+        App.table.make($("#field"));
+        App.field = new Field();
+        App.field.addCurrentTetrimino(new Tetrimino("T", new Vector2(0, 0)));
+        App.table.show(App.field);
+        console.log("done");
     }
 }
+
+// フィールドを表示するテーブルのクラス
+class FieldTable{
+    // int sizeX: 横幅
+    // int sizeY: 縦幅
+    constructor(sizeX, sizeY){
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.cells = [];
+    }
+
+    // フィールドのテーブルを作成、描画する。
+    // jQueryObject jqueryObj: 描画するTable要素のjQueryオブジェクト
+    make(jqueryObj){
+        jqueryObj
+            .append("<tbody id=\"fieldTableBody\" />")
+            .css({ "border-collapse": "collapse" });
+        var tbody = $("#fieldTableBody");
+        var row = null;
+        for(var y = this.sizeY - 1; y >= 0; y--){
+            this.cells[y] = [];
+            tbody.append("<tr id=\"row{0}\" />".format(y));
+            row = $("#row{0}".format(y));
+            for(var x = 0; x < this.sizeX; x++){
+                row.append("<td id=\"r{0}c{1}\" />".format(y, x));
+                this.cells[y][x] = $("#r{0}c{1}".format(y, x));
+            }
+        }
+        this.doToAllCell((obj, y, x)=>{
+            obj.css({
+                "border": "1px #000000 solid",
+                "height": "32px",
+                "width": "32px"
+            });
+        })
+    }
+
+    // 全てのセルに対して処理をする。
+    // function(jQueryObject jqueryObj, int y, int x) func: 実行する処理
+    doToAllCell(func){
+        this.cells.forEach((valueRow, indexY)=>{
+            valueRow.forEach((valueColumn, indexX)=>{
+                func(valueColumn, indexY, indexX);
+            })
+        })
+    }
+
+    // 画面を描画する。
+    // Field field: 描画したいフィールドのオブジェクト
+    show(field){
+        this.doToAllCell((obj, indexY, indexX)=>{
+            if(field.field[indexY][indexX] != undefined){
+                obj.css({ "background-color": Colors[field.field[indexY][indexX].color] });
+            }
+        })
+    }
+}
+
+// フィールドのクラス
+class Field{
+    constructor(){
+        this.sizeX = 10;
+        // テトリミノの出現位置を確保するためにsizeYを24にする。
+        this.sizeY = 24;
+        this.field = [];
+        for(var y = 0; y < this.sizeY; y++){
+            this.field[y] = [];
+            for(var x = 0; x < this.sizeX; x++){
+                this.field[y][x] = new Block(false);
+            }
+        }
+        this.currentTetrimino = null;
+    }
+
+    // 落ちてくるテトリミノを追加します。
+    // Tetrimino tetrimino: 追加するテトリミノのオブジェクト
+    addCurrentTetrimino(tetrimino){
+        this.currentTetrimino = tetrimino;
+        this.expantionTetrimino(this.currentTetrimino);
+    }
+
+    // テトリミノをフィールド上に展開する。
+    expantionTetrimino(tetrimino){
+        var points = tetrimino.getBlockLocations();
+        points.forEach((value)=>{
+            if(this.field[value.y] != undefined && this.field[value.x] != undefined){
+                this.field[value.y][value.x] = new Block(true, tetrimino.block, false);
+            }
+        });
+    }
+
+    // ロックダウンしていないブロックをすべて消す。
+    removeAirBlock(){
+        this.doToAllblock((block, indexY, indexX)=>{
+            this.field[indexY][indexX] = block.isRockedDown ? block : new Block(false);
+        });
+    }
+
+    // 全てのブロックに対して処理をする。
+    // function(Block block, int y, int x) func: 実行する処理
+    doToAllblock(func){
+        this.field.forEach((valueRow, indexY)=>{
+            valueRow.forEach((valueColumn, indexX)=>{
+                func(valueColumn, indexY, indexX);
+            })
+        })
+    }
+
+    downCurrentTetrimino(){
+        this.currentTetrimino.move("down");
+        this.removeAirBlock();
+        this.expantionTetrimino(this.currentTetrimino);
+    }
+}
+
+// フィールドに表示されるブロックのクラス
+class Block{
+    // bool isBlock: ブロックであるかどうか
+    // string color: Colorsで定義された色を示す文字列
+    // bool isRockedDown 既にロックダウンしているかどうか
+    constructor(isBlock, color = "default", isRockedDown = false){
+        this.isBlock = isBlock;
+        this.color = color;
+        this.isRockedDown = isRockedDown;
+    }
+
+    // isRockedDownをtrueに設定する。
+    rockDown(){
+        if(isBlock){
+            this.isRockedDown = true;
+        }else{
+            throw new Error("ブロックが存在しないマスをRockDownすることはできません。");
+        }
+    }
+}
+
+// テトリミノのクラス
+class Tetrimino{
+    // string block: ブロックの種類を示す文字
+    // int rotate: 回転された状態のテトリミノを生成する場合は指定する
+    // Vector2 location: ブロックの基準点の座標
+    constructor(block, location, rotate = 0){
+        this.block = block;
+        this.location = location;
+        this.rotatePatternNum = TetriminoForms[block].length;
+        this.rotate = rotate % this.rotatePatternNum;
+        this.blockLocations = TetriminoForms[block][this.rotate];
+    }
+
+    getBlockLocations(originPoint = null){
+        var origin = originPoint == null ? this.location : originPoint;
+        var rets = [];
+        this.blockLocations.forEach((value)=>{
+            var location = Object.assign({}, value);
+            location.x += origin.x;
+            location.y += origin.y;
+            rets.push(location);
+        });
+        return rets;
+    }
+
+    // テトリミノの位置を動かす
+    // string direction: up, down, right, leftのいずれか
+    // int distance: 何マス動かすか
+    move(direction, distance = 1){
+        if(direction == "up" || direction == "down"){
+            this.location.y += direction == "up" ? distance : distance * -1;
+        }
+        if(direction == "right" || direction == "left"){
+            this.location.x += direction == "right" ? distance : distance * -1;
+        }
+    }
+}
+
+// 二次元空間上での距離のクラス
+class DistanceVector2{
+    // int x: x軸の距離
+    // int y: y軸の距離
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+// 二次元空間上での座標を表すクラス
+class Vector2{
+    // int x: xの座標
+    // int y: yの座標
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+// ブロックの形を定義
+// 出現エリアの左下を原点とした場合のそこからの距離
+// 回転された場合の形を反時計回りで配列としてそれぞれ定義する
+var TetriminoForms = {
+    "I": [ 
+        [
+            new DistanceVector2(0, 0),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0),
+            new DistanceVector2(3, 0)
+        ],
+        [
+            new DistanceVector2(2, 1),
+            new DistanceVector2(2, 0),
+            new DistanceVector2(2, -1),
+            new DistanceVector2(2, -2)
+        ]
+    ],
+    "O": [
+        [
+            new DistanceVector2(1, 1),
+            new DistanceVector2(2, 1),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0)
+        ]
+    ],
+    "S": [
+        [
+            new DistanceVector2(0, 0),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(2, 1)
+        ],
+        [
+            new DistanceVector2(0, 2),
+            new DistanceVector2(0, 1),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 0)
+        ]
+    ],
+    "Z": [
+        [
+            new DistanceVector2(0, 1),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0)
+        ],
+        [
+            new DistanceVector2(2, 2),
+            new DistanceVector2(2, 1),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 0)
+        ]
+    ],
+    "J": [
+        [
+            new DistanceVector2(0, 1),
+            new DistanceVector2(0, 0),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0)
+        ],
+        [
+            new DistanceVector2(0, 0),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 2)
+        ],
+        [
+            new DistanceVector2(0, 1),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(2, 1),
+            new DistanceVector2(2, 0)
+        ],
+        [
+            new DistanceVector2(1, 0),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 2),
+            new DistanceVector2(2, 2)
+        ]
+    ],
+    "L": [
+        [
+            new DistanceVector2(0, 0),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0),
+            new DistanceVector2(2, 1)
+        ],
+        [
+            new DistanceVector2(0, 2),
+            new DistanceVector2(1, 2),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 0)
+        ],
+        [
+            new DistanceVector2(0, 0),
+            new DistanceVector2(0, 1),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(2, 1)
+        ],
+        [
+            new DistanceVector2(1, 2),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0)
+        ]
+    ],
+    "T": [
+        [
+            new DistanceVector2(0, 0),
+            new DistanceVector2(1, 0),
+            new DistanceVector2(2, 0),
+            new DistanceVector2(1, 1)
+        ],
+        [
+            new DistanceVector2(0, 1),
+            new DistanceVector2(1, 2),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 0)
+        ],
+        [
+            new DistanceVector2(0, 1),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(2, 1),
+            new DistanceVector2(1, 0)
+        ],
+        [
+            new DistanceVector2(1, 0),
+            new DistanceVector2(1, 1),
+            new DistanceVector2(1, 2),
+            new DistanceVector2(2, 1)
+        ]
+    ]
+}
+
+// 色
+var Colors = {
+    "default": "#ffffff",
+    "I": "#03A9F4",
+    "O": "#FFEB3B",
+    "S": "#8BC34A",
+    "Z": "#FF5722",
+    "J": "#3F51B5",
+    "L": "#FF9800",
+    "T": "#673AB7"
+};
 
 App.main();
